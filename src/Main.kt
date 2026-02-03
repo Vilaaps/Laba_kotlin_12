129946eead6b02b312d91b8ecf0d87d23ff1370d
Olesya <lesa76632@gmail.com>
2026-02-03 18:12:41 +0300

    docs: add delegation, lazy, observer and state savingdescription

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,19 @@
-import modules.OutpostResource
-import modules.ResourceManager
-
+//import modules.OutpostResource
+//import modules.ResourceManager
 //fun main() {
-    //val manager = ResourceManager()
-    //val minerals = OutpostResource(id = 1, name = "Minerals", amount = 300)
-   // val gas = OutpostResource(id = 2, name = "Gas", amount = 100)
-    //manager.add(minerals)
-    //manager.add(gas)
-    //manager.printAll()
-    //val bonus = minerals.copy(amount = minerals.amount + 50)
-        //println("Копия минералов с бонусом: $bonus")
+//    val manager = ResourceManager()
+//    val minerals = OutpostResource(id = 1, name = "Minerals", amount = 300)
+//    val gas = OutpostResource(id = 2, name = "Gas", amount = 100)
+//    manager.add(minerals)
+//    manager.add(gas)
+//    manager.printAll()
+//    val bonus = minerals.copy(amount = minerals.amount + 50)
+//    println("Копия минералов с бонусом: $bonus")
+//    val loadedResources = FileStorage.load()
+//    loadedResources.forEach {manager.add(it)}
+//    if (loadedResources.isEmpty()) {
+//        manager.add(OutpostResource(1,"Minerals",300))
+//        manager.add(OutpostResource(2,"GAS",100))
+//    }
+//    FileStorage.save(manager.getAll())
 //}
